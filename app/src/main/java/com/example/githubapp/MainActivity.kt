55515c2d898355fb31d9d8ad0d56688338df2abb
icarusdevv <icarocampos@proton.me>
2023-09-12 17:27:25 -0300

    navigation implemented

---
+++ app/src/main/java/com/example/githubapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.githubapp
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.githubapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController : NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostContainer = supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+
+        navController = navHostContainer.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf()
+        )
+    }
+}
